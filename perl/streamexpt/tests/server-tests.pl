22d354b90c93110c23122a68f5cb45246db87dee
oletalk <oletalk@gmail.com>
2013-03-10 21:52:14 +0000

    couple of bad-input tests

--- perl/streamexpt/tests/server-tests.pl
+++ perl/streamexpt/tests/server-tests.pl
@@ -1,6 +1,6 @@
 use lib '..';
 use strict;
-use Test::More tests => 5;
+use Test::More tests => 9;
 
 use HTTP::Daemon;
 
@@ -27,7 +27,7 @@ sleep 1;
 my $pid;
 die "Can't fork: $!" unless defined ($pid = fork() );
 if ($pid == 0) {
-	my $conns = 4;
+	my $conns = 7;
 	while ($conns > 0) {
 		my $conn = $d->accept;
 		$server->process( $conn, $port );
@@ -44,6 +44,15 @@ is($links[4], '/play/third.mp3', 'links for mp3s present');
 
 $local = TestUtils::getlocal($port, '/drop');
 ok( TestUtils::compare_result($local, 'tests/results/server-tests-playlist.dat'), 'generated playlist as expected');
+
+$local = TestUtils::getlocal($port, '/bogus');
+is($local, undef, "bogus request returns no usable data");  # well, a 400 but LWP::Simple can't get at that
+
+$local = TestUtils::getlocal($port, '/play/abcdefg.mp3');
+is($local, undef, "bogus play request returns no usable data"); # a 404
+
+$local = TestUtils::getlocal($port, "/sdlfkjsflksjdfldsjfljkfdlsjfklsjflsjfalkjsflksjfklsjfdslkjslkjsklfjslfkjslfsjlksdjskdlfjsdklfjsdlkfjlfkjweofjewiofjeiofjewoifjfeiof/fe/fejfjfsfs/fjesfsfse/f/sefse/fsefsje/fsefosjf3f32rf02j302jr20r203j3r20jr32jr302jwej\x02sdflkdsfkssdlfkjsflksjdfldsjfljkfdlsjfklsjflsjfalkjsflksjfklsjfdslkjslkjsklfjslfkjslfsjlksdjskdlfjsdklfjsdlkfjlfkjweofjewiofjeiofjewoifjfeiof/fe/fejfjfsfs/fjesfsfse/f/sefse/fsefsje/fsefosjf3f32rf02j302jr20r203j3r20jr32jr302jwej\x02sdflkdsfkssdlfkjsflksjdfldsjfljkfdlsjfklsjflsjfalkjsflksjfklsjfdslkjslkjsklfjslfkjslfsjlksdjskdlfjsdklfjsdlkfjlfkjweofjewiofjeiofjewoifjfeiof/fe/fejfjfsfs/fjesfsfse/f/sefse/fsefsje/fsefosjf3f32rf02j302jr20r203j3r20jr32jr302jwej\x02sdflkdsfkssdlfkjsflksjdfldsjfljkfdlsjfklsjflsjfalkjsflksjfklsjfdslkjslkjsklfjslfkjslfsjlksdjskdlfjsdklfjsdlkfjlfkjweofjewiofjeiofjewoifjfeiof/fe/fejfjfsfs/fjesfsfse/f/sefse/fsefsje/fsefosjf3f32rf02j302jr20r203j3r20jr32jr302jwej\x02sdflkdsfkssdlfkjsflksjdfldsjfljkfdlsjfklsjflsjfalkjsflksjfklsjfdslkjslkjsklfjslfkjslfsjlksdjskdlfjsdklfjsdlkfjlfkjweofjewiofjeiofjewoifjfeiof/fe/fejfjfsfs/fjesfsfse/f/sefse/fsefsje/fsefosjf3f32rf02j302jr20r203j3r20jr32jr302jwej\x02sdflkdsfks");
+is($local, undef, "looooong bogus URI in request returns no usable data"); # a 404
 
 $local = TestUtils::getlocal($port, '/stats');
 ok( TestUtils::compare_result($local, 'tests/results/server-tests-stats.dat'), 'stats URL does something');
