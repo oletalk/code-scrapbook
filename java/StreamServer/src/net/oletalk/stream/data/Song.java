3dc020514fa8f65f9f12c4a1ff1f8b5c705480cc
oletalk <oletalk@gmail.com>
2013-08-17 00:22:56 +0100

    TagDAO done.  Working on populating tags in db.  TODO: timing of this population

--- java/StreamServer/src/net/oletalk/stream/data/Song.java
+++ java/StreamServer/src/net/oletalk/stream/data/Song.java
@@ -8,12 +8,14 @@ import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import net.oletalk.stream.util.LogSetup;
+import net.oletalk.stream.util.TagReader;
 
 /**
  *
@@ -22,6 +24,15 @@ import net.oletalk.stream.util.LogSetup;
 public class Song {
     
     private Path path;
+    private Tag tag;
+
+    public Tag getTag() {
+        return tag;
+    }
+
+    public void setTag(Tag tag) {
+        this.tag = tag;
+    }
     private static final Logger LOG = LogSetup.getlog();
     
     public Song (Path path)
@@ -60,9 +71,44 @@ public class Song {
         }
     }
     
+    public String htmlValue(Path rootpath)
+    {
+        String st = this.pathFrom(rootpath);
+            // paths with accents in them don't work
+        String st_enc = URLEncoder.encode(st);
+
+        return "<a href=\"/play/" + st_enc + "\">" + st + "<br/>\n";
+    }
+    
+    public String m3uValue(String hostheader, Path rootpath)
+    {
+        StringBuilder sb = new StringBuilder();
+        Path p = rootpath.relativize(path);
+        String pathstr = hostheader + p.toString();
+        if (tag != null)
+        {
+            pathstr = tag.m3uvalue() + "\n" + pathstr;
+        }
+        else
+        {
+            pathstr = "#EXTINF:-1," + path.getFileName().toString() + pathstr;
+        }
+        return pathstr;
+    }
+    
     @Override
     public String toString()
     {
-        return "Song: " + path.toString();
+        StringBuilder sb = new StringBuilder();
+        sb.append("Song: ").append(path.toString());
+        if (tag != null)
+        {
+            sb.append("\nwith Tag:").append(tag.toString());        
+        }
+        return sb.toString();
+    }
+
+    public void populateTag() {
+        setTag(TagReader.get(path));
     }
 }
