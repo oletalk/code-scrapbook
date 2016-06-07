b8515109c9339e3901f1a05e3ab4065967a1dfdc
Colin M <oletalk@gmail.com>
2016-06-07 22:21:19 +0100

    IP Whitelisting/blacklisting plus missed out formatting utils

--- ruby/streamtest/StreamServer.rb
+++ ruby/streamtest/StreamServer.rb
@@ -4,6 +4,7 @@ require_relative 'util/config'
 require_relative 'util/db'
 require_relative 'util/player'
 require_relative 'text/format'
+require_relative 'util/ipwl'
 
 configure do
     set :bind, MP3S::Config::SERVER_HOST
@@ -11,6 +12,8 @@ configure do
 
     $logger = Logger.new(STDOUT) # non production
     $logger.level = Logger::INFO
+
+    $ipwl = IPWhitelist.new(MP3S::Clients::List, MP3S::Clients::Default)
 end
 
 get '/' do
@@ -21,16 +24,26 @@ get '/play/:hash' do |req_hash|
     # go find the song with this hash - see _pgtest.rb
     song_loc = Db.find_song(req_hash)
 
+    # TODO: might want to invoke this check in some sort of area common to ALL requests?
+    remote_ip = request.env['REMOTE_ADDR']
+    action = $ipwl.action(remote_ip)
+    $logger.info("Action for #{remote_ip} is #{action}.")
+
     # stream song in output to client
     $logger.info("Song '#{song_loc}' (#{req_hash}) requested")
     if song_loc.nil?
         "404 Sorry, that song was not found"
+    elsif action[:allow].false?
+        "403 Access denied"
     else
         response.headers['Cache-Control'] = 'no-cache'
 
         # if downsampling - PLAY_DOWNSAMPLED_MP3
-        command = MP3S::Config::PLAY_RAW
-        #command = MP3S::Config::PLAY_DOWNSAMPLED_MP3
+        if action[:downsample].true?
+            command = MP3S::Config::PLAY_DOWNSAMPLED_MP3
+        else
+            command = MP3S::Config::PLAY_RAW
+        end
         $logger.info("Fetched command template, #{command}")
 
         played = Player.play_song(command, song_loc)
---
+++ ruby/streamtest/text/format.rb
@@ -0,0 +1,16 @@
+module Format
+    def self.html_list(songlist)
+        ret = songlist.collect{ |song| 
+            %{ <a href='/play/#{song[:hash]}'>#{song[:title]}</a> }
+        }.join("<br/>\n")
+        ret
+    end
+
+    def self.play_list(songlist, hdr_http_host)
+        ret = "#EXTM3U\n"
+        ret << songlist.collect{ |song|
+            "#EXTINF:#{song[:secs]},#{song[:title]}\n" +
+            "http://#{hdr_http_host}/play/#{song[:hash]}"
+        }.join("\n")
+    end
+end
