2ecc5165db1ed6fa7e9c931ee2c7f1ce610e0a29
Colin M <oletalk@gmail.com>
2016-06-12 01:09:05 +0100

    finish up on downsampling

--- ruby/streamtest/StreamServer.rb
+++ ruby/streamtest/StreamServer.rb
@@ -33,17 +33,13 @@ get '/play/:hash' do |req_hash|
     $logger.info("Song '#{song_loc}' (#{req_hash}) requested")
     if song_loc.nil?
         "404 Sorry, that song was not found"
-    elsif action[:allow].false?
+    elsif !action[:allow]
         "403 Access denied"
     else
         response.headers['Cache-Control'] = 'no-cache'
 
-        # if downsampling - PLAY_DOWNSAMPLED_MP3
-        if action[:downsample].true?
-            command = MP3S::Config::PLAY_DOWNSAMPLED_MP3
-        else
-            command = MP3S::Config::PLAY_RAW
-        end
+        # how should we play this song? if client list says downsample, do it
+        command = Player.get_command(action[:downsample], song_loc)
         $logger.info("Fetched command template, #{command}")
 
         played = Player.play_song(command, song_loc)
@@ -61,7 +57,9 @@ get '/list/:spec' do
     # list all the mp3s in the system which match the given spec
     # basically MP3S_ROOT/{foo}
     spec = params['spec']
-    "You want all the songs under the top level folder#{spec}!"
+    if spec == 'all'
+        spec = ''
+    end
     song_list = Db.list_songs("#{MP3S::Config::MP3_ROOT}/#{spec}")
     Format.html_list(song_list)
 end
@@ -70,6 +68,9 @@ get '/m3u/:spec' do
     # list all the mp3s in the system which match the given spec
     # basically MP3S_ROOT/{foo}
     spec = params['spec']
+    if spec == 'all'
+        spec = ''
+    end
     song_list = Db.list_songs("#{MP3S::Config::MP3_ROOT}/#{spec}")
     response.headers['Content-Type'] = 'text/plain'
     Format.play_list(song_list, request.env['HTTP_HOST'])
--- ruby/streamtest/util/player.rb
+++ ruby/streamtest/util/player.rb
@@ -1,6 +1,7 @@
 require 'shellwords'
 require 'open3'
 require 'logger'
+require_relative 'config'
 
 module Player
 
@@ -13,5 +14,23 @@ module Player
         $logger.info("Command to send: #{cmdexec}")
         stdout, stderr, status = Open3.capture3("#{cmdexec}", binmode: true)
         { songdata: stdout, command: cmdexec, warnings: stderr }
+    end
+
+    def self.get_command(downsample, song)
+        $logger = Logger.new(STDOUT)
+        if downsample
+            # check if downsampling mp3/ogg
+            if ( song =~ /mp3$/i )
+                command = MP3S::Config::PLAY_DOWNSAMPLED_MP3
+            elsif ( song =~ /ogg$/i )
+                command = MP3S::Config::PLAY_DOWNSAMPLED_OGG
+            else
+                $logger.error ("No idea how to downsample given file type #{song}")
+                command = nil
+            end
+        else
+            command = MP3S::Config::PLAY_RAW
+        end
+        command
     end
 end
