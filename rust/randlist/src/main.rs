a61537e7f19645a4f9b860a4fedca3aa0be0617f
Colin M <oletalk@gmail.com>
2017-04-09 20:40:42 +0100

    create a function with lifetimes

--- rust/randlist/src/main.rs
+++ rust/randlist/src/main.rs
@@ -3,14 +3,44 @@ extern crate clap;
 use std::fs::File;
 use std::io::Read;
 use std::collections::HashMap;
-use clap::App;
+use clap::{ArgMatches, App};
 use rand::{thread_rng, Rng};
+
+struct RandlistDetails<'a> {
+    source: &'a str,
+    omitfile: &'a str,
+    listsize: i32,
+    verbose: bool
+}
 
 fn filecontents(filename: &str) -> Result<String,std::io::Error> {
     let mut data = String::new();
     let mut f = try!(File::open(filename));
     try!(f.read_to_string(&mut data));
     Ok(data)
+}
+
+fn get_args<'a>(matches: &'a ArgMatches) -> RandlistDetails<'a> {
+    let source_arg = matches.value_of("source").unwrap();
+    let omitfile_arg = matches.value_of("omitfile").unwrap_or("");
+    let listsize_arg = matches.value_of("listsize").unwrap_or("");
+    let verbose_arg = match matches.occurrences_of("verbose") {
+        0 => false,
+        _ => true
+    };
+    RandlistDetails {
+        source: source_arg,
+        omitfile: omitfile_arg,
+        listsize: match listsize_arg {
+            "" => -1,
+            _ => match listsize_arg.to_string()
+                .parse::<i32>() {
+                        Ok(data) => { data }
+                        Err(f) => { panic!(format!("Invalid list size provided ({})", f.to_string())) }
+                    }
+            },
+        verbose: verbose_arg
+    }
 }
 
 fn main() {
@@ -21,28 +51,40 @@ fn main() {
                     .about("Shuffles a playlist")
                     .args_from_usage(
                         "-s, --source=[SOURCEFILE] 'Sets the source file containing song file paths'
-                         -o, --omitfile=[OMITFILE] 'List of songs to omit from the generated list'")
+                         -o, --omitfile=[OMITFILE] 'List of songs to omit from the generated list'
+                         -l, --listsize=[SIZE] 'Number of songs to extract'
+                         -v, --verbose 'Sets verbose mode'")
                     .get_matches();
-    let source_arg = matches.value_of("source").unwrap();
-    let omitfile_arg = matches.value_of("omitfile").unwrap_or("");
+    let app_options = get_args(&matches);
 
     // read songfile - unwrap to a panic! is fine...
-    let songfile = filecontents(source_arg).unwrap();
+    let songfile = filecontents(app_options.source).unwrap();
     let mut lines: Vec<&str> = songfile.lines().collect();
-    println!("I got {} lines from your source file.", lines.len());
+    if app_options.verbose {
+        println!("I got {} lines from your source file.", lines.len());
+    }
 
-    // read omitfile TODO: it's ok if there's none, don't make it panic!
-    let omitfile = match filecontents(omitfile_arg) {
+    // read omitfile. it's ok if there's none, don't make it panic!
+    let omitfile = match filecontents(app_options.omitfile) {
         Ok(data) => data,
         Err(_) => "".to_string()
     };
     let omitcheck = omitfile.lines().map(|line| (line, 1)).collect::<HashMap<_, _>>();
     // spit out randomised lines
     thread_rng().shuffle(lines.as_mut_slice());
-    for line in lines {
-        match omitcheck.get(line) {
-            Some(_) => println!("  (file in omitlist, skipped)"),
-            None => println!("line -> {}", line),
+    
+    let randlist = lines.into_iter().filter(|&x| !omitcheck.contains_key(x));
+    let mut count = 0;
+
+    for line in randlist {
+        println!("{}", line);
+        count = count + 1;
+        if count == app_options.listsize {
+            break;
         }
     }
+    if app_options.verbose {
+        println!("Filtered list is of size {}.", count);
+    }
+
 }
