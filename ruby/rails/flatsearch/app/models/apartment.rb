6f87fc64a9b5a7f9666683c995a54c3c1d997cb9
Colin M <oletalk@gmail.com>
2017-11-25 23:07:57 +0000

    remove other unused rails app

--- ruby/rails/flatsearch/app/models/apartment.rb
+++
@@ -1,7 +0,0 @@
-class Apartment < ActiveRecord::Base
-	after_initialize :init
-
-	def init
-		self.active = true if self.active.nil?
-	end
-end
--- ruby/rails/flatsearch/test/controllers/apartments_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ApartmentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
