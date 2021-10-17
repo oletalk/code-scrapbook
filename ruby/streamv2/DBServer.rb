d156641c3f00a651452a2ef13dd9c109e42f51b3
Colin M <oletalk@gmail.com>
2021-10-17 15:09:25 +0100

    switch around /tags/[song] and /tags/list

--- ruby/streamv2/DBServer.rb
+++ ruby/streamv2/DBServer.rb
@@ -149,14 +149,15 @@ class DBServer < Sinatra::Base
     end
   end
 
-  get '/tags/:hash' do |hash|
-    song_tags = @db.tags_for_song(hash)
-    Format.json(song_tags)
-  end
-
+  # put this first so Sinatra finds 'list' queries before specific ones
   get '/tags/list' do
     all_tags = @db.all_tags_list
     Format.json(all_tags)
+  end
+
+  get '/tags/:hash' do |hash|
+    song_tags = @db.tags_for_song(hash)
+    Format.json(song_tags)
   end
 
   get '/tag/:hash' do |hash|
