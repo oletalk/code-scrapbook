eb0bd07db89a11a3748c7e2c489c2d298c8664aa
Colin M <oletalk@gmail.com>
2021-11-14 16:55:24 +0000

    tag editor css changes

--- ruby/streamv2/dev/js/src/app/tags/LiLink.tsx
+++ ruby/streamv2/dev/js/src/app/tags/LiLink.tsx
@@ -13,7 +13,7 @@ export default class LiLink extends React.Component<LiLinkProps> {
 
   render() {
     return (
-      <span className="taglist_item">
+      <span className="taglist-item">
         <a title={this.props.title} className="tag-link" href="#"
           onClick={this.props.callback}>{this.props.text}</a>
       </span>
