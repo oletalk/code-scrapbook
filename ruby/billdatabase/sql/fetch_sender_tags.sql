c53b9e8f1529f5231c9fbaefd2e241c2b44a3228
Colin M <oletalk@gmail.com>
2023-05-02 22:43:57 +0100

    work on document screen

--- ruby/billdatabase/sql/fetch_sender_tags.sql
+++ ruby/billdatabase/sql/fetch_sender_tags.sql
@@ -1,5 +1,6 @@
-select s.id,
-  t.tag_name
+select distinct s.id,
+  t.tag_name,
+  t.color
 from bills.sender s
   left join bills.sender_tag st on s.id = st.sender_id
   join bills.tag_type t on st.tag_id = t.id
