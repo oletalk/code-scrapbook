26eeb75e5b8f2c16c52d2248de1ea4ee574e12b3
Colin M <oletalk@gmail.com>
2023-07-19 22:15:52 +0100

    use cached files we already downloaded

--- ruby/streamv3/stream/stream.rb
+++ ruby/streamv3/stream/stream.rb
@@ -2,7 +2,7 @@
 
 require 'net/sftp'
 require_relative '../constants'
-require_relative '../process/player'
+require_relative './player'
 
 # A class for handling streaming from a file
 class SongStream
@@ -15,18 +15,23 @@ class SongStream
     time_start = Time.now
     # TODO - something better than dumping all the mp3s over the lifetime of the process in 1 big dir!
     tempfile = "#{MP3S::Config::Cache::TEMP_ROOT}/#{File.basename(ftpfile)}"
-    sftp = Net::SFTP.start(MP3S::Config::Sftp::SERVER_HOST, MP3S::Config::Sftp::USER, password: MP3S::Config::Sftp::PASSWORD)
+    unless File.exists?(tempfile)
+      puts "#{tempfile} not found. downloading and downsampling."
+      sftp = Net::SFTP.start(MP3S::Config::Sftp::SERVER_HOST, MP3S::Config::Sftp::USER, password: MP3S::Config::Sftp::PASSWORD)
     
-    # f = File.open(@file, "rb")
-    sftp.download!(ftpfile, tempfile)
-    time_end_download = Time.now
-    puts "Download completed in #{time_end_download - time_start} seconds."
+      # f = File.open(@file, "rb")
+      sftp.download!(ftpfile, tempfile)
+      time_end_download = Time.now
+      puts "Download completed in #{time_end_download - time_start} seconds."
 
-    # TODO - do we want to make downsampling optional?
-    p = Player.new
-    p.play_downsampled(tempfile)
-    # f = File.open(tempfile)
-    # f.read
+      # TODO - do we want to make downsampling optional?
+      p = Player.new
+      p.play_downsampled(tempfile)
+    else
+      puts 'we already have cached file'
+      f = File.open(tempfile)
+      f.read
+    end
   end
   
 end
