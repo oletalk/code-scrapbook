cf2246885e16e5fa402c0c47428e1d778207c0a7
Colin M <oletalk@gmail.com>
2023-08-03 20:27:11 +0100

    more debugging

--- ruby/streamv3/lib/stream/nowplaying.rb
+++ ruby/streamv3/lib/stream/nowplaying.rb
@@ -17,7 +17,8 @@ class NowPlaying
   def playing(ip)
     logger.debug "Comparison of #{@end_time[ip]} <---> #{Time.now}"
     if @end_time.key?(ip) && @end_time[ip] < Time.now
-      @curr_playing[ip] = nil
+      @curr_playing.delete(ip)
+      @end_time.delete(ip)
       logger.debug 'Past end point of song for this ip'
     end
     @curr_playing[ip]&.display_title
@@ -26,13 +27,12 @@ class NowPlaying
   def start(hsong, ip)
     raise 'NowPlaying.start was not passed a HashSong' unless hsong.is_a?(HashSong)
 
-    unless @curr_playing.nil? || Time.now < @end_time[ip]
-      # if it IS true that @curr_playing is not nil AND Time.now is past @end_time
+    if @curr_playing[ip].nil? || Time.now < @end_time[ip]
       # should we log if (we think) the next song was started
       # before the previous one elapsed?
       logger.debug 'Song duration has elapsed'
       logger.debug 'Recording song played'
-      @curr_playing.record_stat
+      @curr_playing[ip].record_stat
     end
     logger.debug "Recording start of play for song #{hsong.display_title} at #{ip}"
     @curr_playing[ip] = hsong
