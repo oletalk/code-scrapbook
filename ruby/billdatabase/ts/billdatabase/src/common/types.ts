eef34cf7dc90aa3528fb4c8712a5ae0b14a3d072
Colin M <oletalk@gmail.com>
2024-06-08 19:45:21 +0100

    refactor types to allow array replacement across account and contact lists

--- ruby/billdatabase/ts/billdatabase/src/common/types.ts
+++ ruby/billdatabase/ts/billdatabase/src/common/types.ts
@@ -1,33 +1,30 @@
-export type TagObject = {
+type Common = {
   json_class: string,
-  id: string,
+  id:string
+}
+
+export type TagObject = Common & {
   description: string,
   color: string,
   changed: boolean
 }
 
-export type AccountInfo = {
-  json_class: string,
+export type AccountInfo = Common & {
   closed: boolean,
-  id: string,
   sender_id: string,
   account_number: string,
   account_details: string,
   comments: string
 }
 
-export type ContactInfo = {
-  json_class: string,
-  id: string,
+export type ContactInfo = Common & {
   sender_id: string,
   name: string,
   contact: string,
   comments: string
 }
 
-export type SenderInfo = {
-  json_class: string,
-  id: string,
+export type SenderInfo = Common & {
   name: string,
   created_at: Date,
   username: string,
@@ -36,4 +33,18 @@ export type SenderInfo = {
   sender_accounts: AccountInfo[],
   sender_contacts: ContactInfo[],
   sender_tags: TagObject[]
+}
+
+type CommonListItem = AccountInfo | ContactInfo | TagObject
+
+export const replaceItemById = (list: CommonListItem[], newItem: CommonListItem) : CommonListItem[] => {
+  let ret: CommonListItem[] = []
+  for (let x = 0; x < list.length; x++) {
+    if (list[x].id === newItem.id) {
+      ret.push(newItem)
+    } else {
+      ret.push(list[x])
+    }
+  }
+  return ret
 }
