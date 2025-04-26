fc9bbe56cb7cc9d7a3f2950ba4f2e73fc18aa0f2
Colin M <oletalk@gmail.com>
2025-04-26 21:11:53 +0100

    test changes

--- ruby/billdatabase/ts/billdatabase/src/tests/App.test.tsx
+++ ruby/billdatabase/ts/billdatabase/src/tests/App.test.tsx
@@ -1,27 +1,36 @@
-import React from 'react';
-import '@testing-library/jest-dom'
+import { expect } from 'vitest'
 import { waitFor } from '@testing-library/react';
 
 import { render, screen } from '@testing-library/react';
 import Main from '../views/Main';
+import { act } from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
 
 test('renders learn react link', async () => {
   
-  render(
-    <BrowserRouter> 
-  <Main />
-  </BrowserRouter>);
+// i get warned 'await has no effect' but it actually waits for the 
+// render and the test passes, so meh
+await act(() => {
+        render(
+      <BrowserRouter> 
+    <Main />
+    </BrowserRouter>)
+
+  }
+)
+
+  
 await new Promise(process.nextTick);
 
+await waitFor(() => {
   const linkElement = screen.getByText(/Maintain bills/i);
   expect(linkElement).toBeInTheDocument();
-  await waitFor(() => {
-    const paymentAlert = screen.getByText(/unpaid/i);
+
+})
+
+  const paymentAlert = screen.getByText(/unpaid/i);
     expect(paymentAlert).toBeInTheDocument();
-  })
+
 
 });
-
-// TODO: add a text that uses fetch
-// more hints in https://kentcdodds.com/blog/stop-mocking-fetch
