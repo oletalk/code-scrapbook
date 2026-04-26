70e8c8b708f8ad47c0ace1da40022ca2c1cfece4
Colin M <oletalk@gmail.com>
2026-04-26 14:17:32 +0100

    bump up vite version

--- ruby/billdatabase/ts/billdatabase/src/components/CheckPayments.tsx
+++ ruby/billdatabase/ts/billdatabase/src/components/CheckPayments.tsx
@@ -1,37 +1,31 @@
-import { doFetch } from "../common/fetch"
-import { PaymentInfo } from "../common/types-class"
-import { BACKEND_URL } from "../common/constants"
-import { useCallback, useState, useEffect } from "react"
+import { doFetch } from "../common/fetch";
+import { PaymentInfo } from "../common/types-class";
+import { BACKEND_URL } from "../common/constants";
+import { useCallback, useState, useEffect } from "react";
 
 function CheckPayments() {
-
-  const [ unpaid, setUnpaid ] = useState<string>()
+  const [unpaid, setUnpaid] = useState<string>();
   const fetchPaymentsDue = useCallback(() => {
-    doFetch<PaymentInfo[]>(BACKEND_URL + '/payments')
-    .then((json) => {
+    doFetch<PaymentInfo[]>(BACKEND_URL + "/payments").then((json) => {
       for (let p of json) {
-        console.log(p.status)
-        if (p.status !== 'paid') {
-          setUnpaid(p.status)
+        console.log(p.status);
+        if (p.status !== "paid") {
+          setUnpaid(p.status);
         }
       }
-    })
-  }, [])
+    });
+  }, []);
 
   useEffect(() => {
-    fetchPaymentsDue()
-  }, [fetchPaymentsDue])
+    fetchPaymentsDue();
+  }, [fetchPaymentsDue]);
 
-  if (typeof unpaid === 'undefined') {
-    return (
-      <span>&nbsp;</span>
-    )
+  if (typeof unpaid === "undefined") {
+    return <span>&nbsp;</span>;
   } else {
-    return (
-      <span className='paymentAlert'>{unpaid}</span>
-    )
+    return <span className="paymentAlert">{unpaid}</span>;
   }
-
 }
 
-export default CheckPayments
+export default CheckPayments;
+
